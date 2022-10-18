438d663491c1691db5a7ba8a7514b34a1ec85687
Alawson7821 <alawson5433@proton.me>
2022-10-18 00:10:38 -0500

    Removed Unecessary file

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,13 @@
   import Timeline from "./lib/timeline.svelte";
   import { gradients } from "./lib/background";
   import EventPoster from './lib/eventPoster.svelte';
-  import { fade } from 'svelte/transition';
-
   let posterVis = false;
 
 </script>
 
 <div id="wrapper">
   <Timeline on:timeBtnHover={() => posterVis = true} on:timeBtnHoverStop={() => posterVis = false}/>
-  <EventPoster posterVis={posterVis} nextPostVis={}/>
+  <EventPoster posterVis={posterVis} nextPostVis={posterVis}/>
 </div>
 
 
